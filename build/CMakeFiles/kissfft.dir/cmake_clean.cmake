3ac2b310848b43cb5f6dd6dfa7061be8ae40fc69
Marc Schmidt <schmidt.2284698@studenti.uniroma1.it>
2026-04-22 19:40:39 +0200

    added build files
    Signed-off-by: Marc Schmidt <schmim73@students.zhaw.ch>

---
+++ build/CMakeFiles/kissfft.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+        "CMakeFiles/kissfft.dir/third_party/kissfft/kiss_fft.c.o"
+        "CMakeFiles/kissfft.dir/third_party/kissfft/kiss_fft.c.o.d"
+        "CMakeFiles/kissfft.dir/third_party/kissfft/kiss_fftr.c.o"
+        "CMakeFiles/kissfft.dir/third_party/kissfft/kiss_fftr.c.o.d"
+        "libkissfft.a"
+        "libkissfft.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach (lang C)
+    include(CMakeFiles/kissfft.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach ()
---
+++ build/CMakeFiles/test_fft.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+        "CMakeFiles/test_fft.dir/src/fft.cpp.o"
+        "CMakeFiles/test_fft.dir/src/fft.cpp.o.d"
+        "CMakeFiles/test_fft.dir/src/signal.cpp.o"
+        "CMakeFiles/test_fft.dir/src/signal.cpp.o.d"
+        "CMakeFiles/test_fft.dir/tests/test_fft.cpp.o"
+        "CMakeFiles/test_fft.dir/tests/test_fft.cpp.o.d"
+        "test_fft"
+        "test_fft.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach (lang CXX)
+    include(CMakeFiles/test_fft.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach ()
