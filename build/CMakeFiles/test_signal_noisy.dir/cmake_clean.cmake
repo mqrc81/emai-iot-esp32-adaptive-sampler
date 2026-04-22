a792f58fbb4fc09e9c20aa833f57501a05c64d33
Marc Schmidt <schmidt.2284698@studenti.uniroma1.it>
2026-04-22 22:32:17 +0200

    Added noisy signal filters
    Signed-off-by: Marc Schmidt <schmim73@students.zhaw.ch>

---
+++ build/CMakeFiles/test_signal_noisy.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_signal_noisy.dir/src/signal.cpp.o"
+  "CMakeFiles/test_signal_noisy.dir/src/signal.cpp.o.d"
+  "CMakeFiles/test_signal_noisy.dir/tests/test_signal_noisy.cpp.o"
+  "CMakeFiles/test_signal_noisy.dir/tests/test_signal_noisy.cpp.o.d"
+  "test_signal_noisy"
+  "test_signal_noisy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_signal_noisy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
